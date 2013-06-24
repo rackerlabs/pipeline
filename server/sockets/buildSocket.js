19fc10c1b216459a804bc0bb5ca5d4b9d5443ae1
Chris Cantu <christopher.cantu@gmail.com>
2013-06-24 14:50:13 -0500

    Try leveraging promises to queue spawning of processes

--- server/sockets/buildSocket.js
+++ server/sockets/buildSocket.js
@@ -1,8 +1,29 @@
 'use strict';
 var spawn = require('child_process').spawn;
+var Q = require('q');
 var Build = require('./../../server/db/schemas').Build; //TODO: Save data to mongodb
 
 module.exports = function( socket ) {
+    var seqSpawn = function(words) {
+        var defer = Q.defer();
+
+        var command = spawn(words.shift(), words);
+
+        command.stdout.on('data', function(data) {
+            socket.emit('builds:update', data.toString());
+        });
+
+        command.stderr.on('data', function(data) {
+            socket.emit('builds:error', data.toString());
+        });
+
+        command.on('close', function(code) {
+            socket.emit('builds:finished', code);
+            defer.resolve(code);
+        });
+
+        return defer.promise;
+    }    
 
     socket.on( 'send:example', function( data ) {
         console.log("begining send:example");
@@ -29,23 +50,16 @@ module.exports = function( socket ) {
                 return;
             };
 
+            var functions = []
             //TODO Ensure commands are executed in order.  
             for (var i = 0; i < build.commands.length; i++) {
                 var words = build.commands[i].command.split(' ');
-                var command = spawn(words.shift(), words);
-
-                command.stdout.on('data', function(data) {
-                    socket.emit('builds:update', data.toString());
-                });
-
-                command.stderr.on('data', function(data) {
-                    socket.emit('builds:error', data.toString());
-                });
-
-                command.on('close', function(code) {
-                    socket.emit('builds:finished', code);
-                });
+                functions.push( seqSpawn(words) );
             };
+
+            functions.reduce(function( sofar, f) {
+                return sofar.then(f);
+            }, Q.resolve(0));
 
         });
 
