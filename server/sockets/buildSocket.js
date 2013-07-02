f5f6f26f33979ba13007c78481e89ac482b926d6
Stephen Golub <stephen.golub@rackspace.com>
2013-07-02 11:00:30 -0500

    FIX Code now executes in sequence

--- server/sockets/buildSocket.js
+++ server/sockets/buildSocket.js
@@ -2,32 +2,37 @@
 var spawn = require('child_process').spawn;
 var Q = require('q');
 var Build = require('./../../server/db/schemas').Build; //TODO: Save data to mongodb
+var _ = require('lodash');
 
 module.exports = function( socket ) {
-    var seqSpawn = function( cmd ) {
-        var defer = Q.defer();
-        var words = cmd.command.split(' ');
-
-        console.log("Started: xxxxxxx " + words + " xxxxxxx");
-
-        var command = spawn(words.shift(), words);
-
-        command.stdout.on('data', function(data) {
-            socket.emit('builds:update', data.toString());
-        });
-
-        command.stderr.on('data', function(data) {
-            socket.emit('builds:error', data.toString());
-            defer.resolve(data);
-        });
-
-        command.on('close', function(code) {
-            console.log("Finished: xxxxxxx " + words + " xxxxxxx");
-            socket.emit('builds:finished', code);
-            defer.resolve(code);
-        });
-
-        return defer.promise;
+    var scope = {};
+    
+    scope.seqSpawn = function( cmd ) {
+        return function () {
+            var defer = Q.defer();
+            var words = cmd.command.split(' ');
+    
+            console.log("Started: xxxxxxx " + words + " xxxxxxx");
+    
+            var command = spawn(words.shift(), words);
+    
+            command.stdout.on('data', function(data) {
+                socket.emit('builds:update', data.toString());
+            });
+    
+            command.stderr.on('data', function(data) {
+                socket.emit('builds:error', data.toString());
+                defer.resolve(data);
+            });
+    
+            command.on('close', function(code) {
+                console.log("Finished: xxxxxxx " + words + " xxxxxxx");
+                socket.emit('builds:finished', code);
+                defer.resolve(code);
+            });
+    
+            return defer.promise;
+        }
     };
 
     socket.on("builds:startBuild", function(data) {
@@ -36,19 +41,18 @@ module.exports = function( socket ) {
                 socket.emit('builds:error', 'Unable to find build by id: ' + data.id);
                 return;
             };
+            
+            var funcs = [];
 
-            var functions = []; 
             for (var i = 0; i < build.commands.length; i++) { 
-                functions.push( seqSpawn );
+                funcs.push( scope.seqSpawn );
             };
 
             var result = Q.resolve(0);
-            var counter = 0;
 
-            functions.forEach( function(f) {
-                result = result.then( f(build.commands[counter]) );
-                counter++;
-            });
+            _.forEach(build.commands, function(c) {
+                result = result.then( this(c) );
+            }, scope.seqSpawn);
         });
     });
 };
