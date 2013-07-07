0b948407e18cafadfbdcef24d3c44d43e6984428
Chris Cantu <christopher.cantu@gmail.com>
2013-07-07 15:36:23 -0500

    Create scaffolding for github api

---
+++ server/routes/github.js
@@ -0,0 +1,29 @@
+'use strict';
+
+exports.createBranch = function (req, res) {
+
+};
+
+exports.listBranches = function (req, res) {
+
+};
+
+exports.listPulls = function (req, res) {
+
+};
+
+exports.createPull = function(req, res) {
+
+};
+
+exports.isPullMergeable = function (req, res) {
+
+};
+
+exports.mergePull = function (req, res) {
+
+};
+
+exports.createTag = function (req, res) {
+
+};
