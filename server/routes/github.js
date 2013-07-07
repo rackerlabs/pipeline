182e4c218558e8070e311750f9641b4d5c1e8f7e
Chris Cantu <christopher.cantu@gmail.com>
2013-07-07 16:06:46 -0500

    Stubbed out the createPull method.

--- server/routes/github.js
+++ server/routes/github.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var rest = require('restler'), Q = require('q');
+var GITHUB_URL = "https://api.github.com/repos/"
+
 exports.createBranch = function (req, res) {
 
 };
@@ -13,7 +16,29 @@ exports.listPulls = function (req, res) {
 };
 
 exports.createPull = function(req, res) {
+	var REPO = { 
+		"owner"		: "SOME_OWNER",
+		"repoName"	: "SOME_REPO",
+		"headers": {
+			"Authorization"	: "token GITHUB API TOKEN",
+			"Accept"		: "application/json"
+		}
+	};
 
+	var data = {
+		"title" : req.branchName,
+		"head"  : req.branchName,
+		"base"	: "master"
+	}, json = JSON.stringify(data);
+
+	return rest.post(GITHUB_URL + REPO.owner + "/" + REPO.repoName + "/pulls", 
+		{ headers : REPO.headers, data : json }
+	).on('success', function (data) {
+		data.msg = "Created pull request for branch: " + req.branchName; 
+		res.json(201, data);
+	}).on('fail', function (data) {
+		res.json(400, data);
+	});
 };
 
 exports.isPullMergeable = function (req, res) {
