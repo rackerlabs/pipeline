23b930d0b3f2ac4b261341e4fe0c653c59df3d72
Chris Cantu <christopher.cantu@gmail.com>
2013-08-02 15:14:40 -0500

    Fixed error in server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ passport.deserializeUser(function(obj, done) {
 //   Strategies in Passport require a `verify` function, which accept
 //   credentials (in this case, an accessToken, refreshToken, and GitHub
 //   profile), and invoke a callback with a user object.
-passport.use(new GitHubStrategy({
+passport.use(new Strategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:" + port + "/auth/callback"
