2d83ef99d644f31c880660ad2044481c973ca868
Chris Cantu <christopher.cantu@gmail.com>
2013-08-02 11:04:19 -0500

    Still incomplete

--- server/server.js
+++ server/server.js
@@ -1,9 +1,5 @@
 'use strict';
 
-/**
- * Module dependencies
- */
-
 var express     = require('express'),
     cons        = require('consolidate'),
     http        = require('http'),
@@ -14,20 +10,66 @@ var express     = require('express'),
     pipeline    = require('./routes/pipeline'),
     build       = require('./routes/build'),
     repo        = require('./routes/repo'),
-    github      = require('./routes/github');
+    github      = require('./routes/github'), 
+    passport    = require('passport'),
+    Strategy    = require('passport-github').Strategy;
+
+var GITHUB_CLIENT_ID = "--insert-github-client-id-here--"
+var GITHUB_CLIENT_SECRET = "--insert-github-client-secret-here--";
+var port = process.env.PORT || appConfig.server.port;
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete GitHub profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the GitHubStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and GitHub
+//   profile), and invoke a callback with a user object.
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:" + port + "/auth/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
 
 // Server instance
 var server = exports.server = express();
 
 // Configure Server
 server.configure( function() {
-    server.set( 'port', process.env.PORT || appConfig.server.port );
+    server.set( 'port', port );
     server.set( 'views', path.join( __dirname, './../app' ) );
     server.engine( 'html', cons.hogan );
     server.set( 'view engine', 'html' );
     server.engine( 'hjs', cons.hogan );
     server.set( 'view engine', 'hjs' );
-
+    server.use( express.session({ secret: '!?phrasing?!' }) );
+    server.use( passport.initialize() );
+    server.use( passport.session() );
     server.use( express.bodyParser() );
     server.use( express.methodOverride() );
     server.use( express.static( path.join( __dirname, './../app' ) ) );
