5498fe62b6ec5dade57da6385fd0c8b2059fa0ac
Chris Cantu <christopher.cantu@gmail.com>
2013-08-25 22:01:56 -0500

    Trying github node api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,26 +1,26 @@
 'use strict';
-var passport = require('passport');
 
 exports.isAuthenticated = function (req, res, next) {
-	return ( req.isAuthenticated() ) ? next() : res.send(401, { msg: "Unauthorized Request"});
+    return req.isAuthenticated() ? next() : res.json(401, { msg: "Unauthorized Request"});
 };
-
-exports.authenticate = function (accessToken, refreshToken, profile, done) {
-	process.nextTick( function () {
-		//TODO: Fetch user and validate user exists
-		return done(null, profile);	
-	});
-};
-
-exports.failCallback = function (req, res) {
-	res.send(401, { msg: "Unauthorized"} );
-};
-
-exports.successCallback = function (req, res) {
-	res.send(200, { msg: "Authenticated"} );
-}
 
 exports.logout = function (req, res) {
-	req.logout();
-	res.send(200, { msg: "Logged Out"});
+    req.logout();
+    res.json(200, { msg: "Logged Out"});
+};
+
+exports.authSuccess = function (req, res) {
+    res.json(200, {'_id': 'myid', firstName: req.body.username, loggedIn: true});
+};
+
+exports.loggedIn = function (req, res) {
+    return (req.isAuthenticated()) ? exports.authSuccess(req, res) : res.json(401, {loggedIn: false});
+};
+
+exports.serialize = function (user, done) {
+    done(null, user._id);
+};
+
+exports.deserialize = function(user, done) {
+    done(null, user);
 };
--- server/server.js
+++ server/server.js
@@ -12,51 +12,29 @@ var express     = require('express'),
     repo        = require('./routes/repo'),
     github      = require('./routes/github'), 
     passport    = require('passport'),
-    Strategy    = require('passport-github').Strategy,
+    Strategy    = require('passport-local').Strategy,
+    GHApi      = require( 'github' ),
     auth        = require('./routes/auth');
 
 var port = process.env.PORT || appConfig.server.port;
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete GitHub profile is serialized
-//   and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-});
-
-// Use the GitHubStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GitHub
-//   profile), and invoke a callback with a user object.
-passport.use(new Strategy({
-    clientID: appConfig.github.clientId,
-    clientSecret: appConfig.github.clientSecret,
-    callbackURL: "http://127.0.0.1:" + port + "/auth/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      
-      // To keep the example simple, the user's GitHub profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the GitHub account with a user record in your database,
-      // and return that user instead.
-      return done(null, profile);
-    });
-  }
-));
-
-
-// Server instance
 var server = exports.server = express();
+var gh = new GHApi({version: "3.0.0"});
+
+var validate = function(username, password) {
+    gh.authenticate({ type: "basic", username: username , password: password });
+};
+
+var authenticate = function(username, password, done) {
+    return validate(username, password) ? done(null, {'_id': 'mongoid'}) : done(null, false, { msg: 'Invalid Credentials'});
+};
+
+var isAuthenticated = function (req, res, next) {
+    return req.isAuthenticated() ? next() : res.json(401, { msg: 'Unauthorized' });
+};
+
+passport.use( new Strategy (authenticate));
+passport.serializeUser( auth.serialize );
+passport.deserializeUser( auth.deserialize );
 
 // Configure Server
 server.configure( function() {
@@ -125,7 +103,7 @@ server.get('/api/github/branches/:repoId', github.listBranches);
 server.post('/api/github/branches/:repoId', github.createBranch);
 server.post('/api/github/tags/:repoId', github.createTag);
 
-server.post('/api/auth', passport.authenticate('github'), auth.successCallback);
+server.post('/api/auth', passport.authenticate('local'), auth.authSuccess);
 
 
 
