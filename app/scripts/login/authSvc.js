7764df6f6ff2c6f82b991879af221b983a3dbb4e
Chris Cantu <christopher.cantu@gmail.com>
2013-12-10 15:38:22 -0600

    Fixed added paramter in api call

--- app/scripts/login/authSvc.js
+++ app/scripts/login/authSvc.js
@@ -1,10 +1,10 @@
 angular.module('rxAuthSvc', ['ngResource'])
     .factory('Auth', function ($resource) {
         return $resource('/api/auth/:action',
-            { id: '@id' },
+            {  },
             {
                 login: { method: 'POST', isArray: false, ignoreAuthModule: true },
                 logout: { method: 'DELETE', isArray: false, ignoreAuthModule: true },
-                loggedIn: { method: 'GET', isArray: false, params: { action: 'loggedIn'} }
+                loggedIn: { method: 'GET', isArray: false, params: { action: 'loggedIn' } }
             });
     });
