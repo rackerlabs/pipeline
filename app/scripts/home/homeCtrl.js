f3c75b6006aee434ca53a47413afa7c1c956d5c7
Chris Cantu <christopher.cantu@gmail.com>
2013-12-10 11:11:01 -0600

    Added redirect to home page when succesfully logged in

---
+++ app/scripts/home/homeCtrl.js
@@ -0,0 +1,4 @@
+angular.module('rxPipelineApp')
+    .controller('HomeCtrl', function ($scope) {
+        console.log($scope);
+    });
--- app/scripts/login/loginCtrl.js
+++ app/scripts/login/loginCtrl.js
@@ -2,7 +2,7 @@ angular.module('rxPipelineApp')
     .controller('LoginCtrl', function ($scope, $location, Auth) {
         $scope.login = function () {
             Auth.login($scope.user, function() {
-                $location.path('/');
+                $location.path('/home');
             }, function () {
                 alert('Failed!');
             });
