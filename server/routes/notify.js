7b5aa255b7c9c46728097c93f9fe2d90d6e0ff1c
Chris Cantu <christopher.cantu@gmail.com>
2013-12-11 23:08:41 -0600

    Fixed jshint errors in notify and buildSocket

--- server/routes/notify.js
+++ server/routes/notify.js
@@ -1,63 +1,63 @@
 'use strict';
+/*jslint es5: true */
 
-var email = require('./../../server/utils/email'), 
-	user = require('./../../server/routes/user'),
-	pipeline = require('./../../server/routes/pipeline'),
-	Q = require('q'),
-	_ = require('lodash');
+var email = require('./../../server/utils/email'),
+    user = require('./../../server/routes/user'),
+    pipeline = require('./../../server/routes/pipeline'),
+    _ = require('lodash');
 
 var options = {
-    from: "Roger's Mom ✔ <christopher.cantu@gmail.com>", // sender address
-    subject: "Open 24/7", // Subject line
-    text: "99% DISCOUNT for all customers", // plaintext body
-    html: "<b>99% Discount</b> for all customers!" // html body
+    from: 'Rogers Mom ✔ <christopher.cantu@gmail.com>', // sender address
+    subject: 'Open 24/7', // Subject line
+    text: '99% DISCOUNT for all customers', // plaintext body
+    html: '<b>99% Discount</b> for all customers!' // html body
 };
 
 exports.emailUser = function (req, res) {
-	var opts = ( req.body.options ) ? req.body.options : options;
+    var opts = ( req.body.options ) ? req.body.options : options;
 
-	user.findById(req.params.id)
-		.then( function(user) {
-			opts.to = user.email;
-			return email.sendMail(options);
-		})
-		.then( function (message) {
-			return res.json(200, message);
-		}).catch( function (error) {
-        	res.json(409, error);
-    	});
+    user.findById(req.params.id)
+        .then( function(user) {
+            opts.to = user.email;
+            return email.sendMail(options);
+        })
+        .then( function (message) {
+            return res.json(200, message);
+        }).catch( function (error) {
+            return res.json(409, error);
+        });
 };
 
 exports.emailUsers = function (req, res) {
-	var usersIds = req.body.ids;
-	var opts = ( req.body.options ) ? req.body.options : options;
+    var usersIds = req.body.ids;
+    var opts = ( req.body.options ) ? req.body.options : options;
 
-	user.findUsersByIds(usersIds)
-		.then( function(users) {
-			var ids = _.pluck(users, 'email');
-			opts.to = ids.toString();
-			return email.sendMail(opts);
-		}).then( function (message) {
-			return res.json(200, message);
-		}).catch( function (error) {
-			return res.json(409, error);
-		});
+    user.findUsersByIds(usersIds)
+        .then( function (users) {
+            var ids = _.pluck(users, 'email');
+            opts.to = ids.toString();
+            return email.sendMail(opts);
+        }).then( function (message) {
+            return res.json(200, message);
+        }).catch( function (error) {
+            return res.json(409, error);
+        });
 };
 
 exports.pipelineEmail = function (buildId) {
-	pipeline.findByBuildIdAndNotificationType(buildId, "email").then( function (pipe) {
-		var notification = _.find(pipe.notifications, function (notify) {
-			return notify.notification_type == "email";
-		});
+    pipeline.findByBuildIdAndNotificationType(buildId, 'email').then( function (pipe) {
+        var notification = _.find(pipe.notifications, function (notify) {
+            return notify.notificationType === 'email';
+        });
 
-		var mail = {
-			from: "Chris Cantu <christopher.cantu@gmail.com>",
-			to: notification.contacts.toString(),
-			text: notification.body,
-			html: notification.body,
-			subject: notification.subject
-		};
+        var mail = {
+            from: 'Chris Cantu <christopher.cantu@gmail.com>',
+            to: notification.contacts.toString(),
+            text: notification.body,
+            html: notification.body,
+            subject: notification.subject
+        };
 
-		return email.sendMail(mail);
-	});
-}
+        return email.sendMail(mail);
+    });
+};
--- server/sockets/buildSocket.js
+++ server/sockets/buildSocket.js
@@ -1,4 +1,6 @@
 'use strict';
+/*jshint unused:true */
+
 var spawn = require('child_process').spawn;
 var Q = require('q');
 var Build = require('./../../server/db/schemas').Build; //TODO: Save data to mongodb
@@ -6,52 +8,52 @@ var _ = require('lodash');
 
 module.exports = function( socket ) {
     var scope = {};
-    
+
     scope.seqSpawn = function( build, counter ) {
         return function () {
-            var defer = Q.defer(), 
+            var defer = Q.defer(),
                 cmd = build.commands[counter],
                 history = {};
 
             var words = cmd.command.split(' ');
             var command = spawn(words.shift(), words);
-    
+
             command.stdout.on('data', function(data) {
                 var dataString = data.toString();
                 socket.emit('builds:update', dataString);
                 history.output = history.output + dataString;
             });
-    
+
             command.stderr.on('data', function(data) {
-                var dataString = data.toString() 
+                var dataString = data.toString();
                 socket.emit('builds:error', dataString);
 
                 history.output = history.output + dataString;
                 history.isSuccessful = false;
             });
-    
+
             command.on('close', function(code) {
                 socket.emit('builds:finished', code);
-                
+
                 history.endTime = new Date();
 
                 build.buildHistory.push(history);
                 build.save();
-                
+
                 defer.resolve(code);
             });
-    
+
             return defer.promise;
-        }
+        };
     };
 
-    socket.on("builds:startBuild", function(data) {
+    socket.on('builds:startBuild', function (data) {
         Build.findById(data.id, function(err, build) {
             if (err || !build) {
                 socket.emit('builds:error', 'Unable to find build by id: ' + data.id);
                 return;
-            };
-            
+            }
+
             var result = Q.resolve(0);
             var counter = 0;
 
