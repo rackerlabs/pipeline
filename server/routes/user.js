b44605d94eeefa311b66c473fe2c62626dbebee2
Chris Cantu <christopher.cantu@gmail.com>
2013-12-11 22:11:05 -0600

    Fixed jshint errors

--- server/routes/user.js
+++ server/routes/user.js
@@ -32,7 +32,7 @@ exports.get = function (req, res) {
     var id = req.params.id;
     
     return User.findOne({_id:id}, function (err, user) {
-        return (err || !user) ? res.json(404, {msg: "Unable to find user with id: " + id}) : res.json(200, user);
+        return (err || !user) ? res.json(404, {msg: 'Unable to find user with id: ' + id}) : res.json(200, user);
     });
 };
 
@@ -40,8 +40,8 @@ exports.save = function (req, res) {
     var myUser = new User(req.body);
     
     return myUser.save( function (err, user) {
-        return (!err) ? res.json(200, user) : res.json(400, {msg: "Unable to create new user"});
-    });    
+        return (!err) ? res.json(200, user) : res.json(400, {msg: 'Unable to create new user'});
+    });
 };
 
 exports.update = function (req, res) {
@@ -50,7 +50,7 @@ exports.update = function (req, res) {
     body.lastUpdated = new Date();
     
     return User.update({_id: id}, body, {}, function (err, updatedNumber, raw) {
-        return (!err) ?  res.json(200, raw) : res.json(400, {msg: "Unable to update user id: " + id });
+        return (!err) ?  res.json(200, raw) : res.json(400, {msg: 'Unable to update user id: ' + id });
     });
 };
 
@@ -58,20 +58,21 @@ exports.delete = function (req, res) {
     var id = req.params.id;
     
     return User.remove({ _id: id}, function (err) {
-        return (!err) ? res.json(200, {msg: "Deleted user id: " + id}) : res.json(400, {msg: "Unable to delete user id: " + id});
+        return err ? res.json(400, {msg: 'Unable to delete user id: ' + id}) :
+            res.json(200, {msg: 'Deleted user id: ' + id});
     });
 };
 
-var saveUser = function(user) {
-	return new User(user).save( function(err, user) {
-		return err ? new Error(err) : null;
+var saveUser = function(obj) {
+	return new User(obj).save( function(err, user) {
+		return err ? new Error(err) : user;
 	});
 };
 
-exports.bootstrap = function () { 
-	saveUser( { createdBy: "christophercantu", username: "christophercantu", email: 'chris.cantu@rackspace.com'} );
-	saveUser( { createdBy: "christophercantu", username: "nickburns2006",  email: 'stephen.golub@rackspace.com' });
-	saveUser( { createdBy: "christophercantu", username: "rnreekez", email: 'roger.enriquez@rackspace.com'});
-	saveUser( { createdBy: "christophercantu", username: "hussamd", email: 'hussam.dawood@rackspace.com'});
-    saveUser( { createdBy: "christophercantu", username: "rsyed83", email: 'rahman.syed@rackspace.com'} );
-}
+exports.bootstrap = function () {
+	saveUser( { createdBy: 'christophercantu', username: 'christophercantu', email: 'chris.cantu@rackspace.com'} );
+	saveUser( { createdBy: 'christophercantu', username: 'nickburns2006',  email: 'stephen.golub@rackspace.com' });
+	saveUser( { createdBy: 'christophercantu', username: 'rnreekez', email: 'roger.enriquez@rackspace.com'});
+	saveUser( { createdBy: 'christophercantu', username: 'hussamd', email: 'hussam.dawood@rackspace.com'});
+    saveUser( { createdBy: 'christophercantu', username: 'rsyed83', email: 'rahman.syed@rackspace.com'} );
+};
