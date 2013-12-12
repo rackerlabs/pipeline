db6a671865ce87f67ffc9462a11cdea331aed32b
Chris Cantu <christopher.cantu@gmail.com>
2013-12-12 12:52:22 -0600

    Added ability to delete pipelines

--- app/scripts/home/homeCtrl.js
+++ app/scripts/home/homeCtrl.js
@@ -1,8 +1,18 @@
 angular.module('rxPipelineApp')
-    .controller('HomeCtrl', function ($scope, Pipeline) {
+    .controller('HomeCtrl', function ($scope, $window, Pipeline) {
         $scope.pipelines = Pipeline.list();
 
-        $scope.deletePipeline = function (pipelineId){
-            console.log(pipelineId);
+        $scope.deletePipeline = function (pipelineId) {
+            if ( !$window.confirm('Are you sure you want to remove this Pipeline') ) {
+                return;
+            }
+
+            Pipeline.remove( {id: pipelineId},
+                function () {
+                    $scope.pipelines = Pipeline.list();
+                }, function () {
+                    //TODO change this to an alert message
+                    alert('Unable to delete pipeline');
+                });
         };
     });
