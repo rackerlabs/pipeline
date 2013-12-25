46273701a427dbb2da1f454d22bff0b71dc217f5
Chris Cantu <christopher.cantu@gmail.com>
2013-12-25 09:05:25 -0600

    Made default task run the server in development mode.

--- tasks/default.js
+++ tasks/default.js
@@ -2,10 +2,7 @@ module.exports = function (grunt) {
     grunt.registerTask('default',
         'Default task will lint code, run unit, mid and functional tests, build code and generate docs.',
         [
-            'jshint',
-            'karma:full',
-            'build',
-            'docs'
+            'server'
         ]
     );
 };
